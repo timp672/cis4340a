8920de10d7bde51c463f1642681bea8269fc63be
timp672 <90533976+timp672@users.noreply.github.com>
2024-03-02 18:13:24 -0500

    Update R00_IDS03_J.java

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,9 +1,8 @@
 public class R00_IDS03_J {
     public static void main(String [] args){
-        if (loginSuccessful) {
-            logger.severe("User login succeeded for: " + username);
-          } else {
-            logger.severe("User login failed for: " + username);
-          }
+    if (loginSuccessful) {
+      logger.severe("User login succeeded for: " + sanitizeUser(username));
+    } else {
+      logger.severe("User login failed for: " + sanitizeUser(username));
     }
 }
