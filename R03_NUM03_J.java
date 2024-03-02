fe1096220df150c6ec888fde7e279020768d13bf
timp672 <90533976+timp672@users.noreply.github.com>
2024-03-02 18:27:25 -0500

    Update R03_NUM03_J.java

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 
 public class R03_NUM03_J {
     
-    public static int getInteger(DataInputStream is) throws IOException {
-        return is.readInt(); 
+    public static long getInteger(DataInputStream is) throws IOException {
+          return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bits
     }
 }
