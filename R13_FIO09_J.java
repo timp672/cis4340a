76e0fa3a4c822d018446e063a6f8b9f6a94e7e8f
timp672 <90533976+timp672@users.noreply.github.com>
2024-03-02 18:51:01 -0500

    Update R13_FIO09_J.java

--- R13_FIO09_J.java
+++ R13_FIO09_J.java
@@ -1,11 +1,17 @@
 public class R13_FIO09_J {
     
-    class ConsoleWrite {
-        public static void main(String[] args) {
-          // Any input value > 255 will result in unexpected output
-          System.out.write(Integer.valueOf(args[0]));
-          System.out.flush();
+    class FileWrite {
+      public static void main(String[] args)
+                              throws NumberFormatException, IOException {
+        // Perform range checking 
+        int value = Integer.valueOf(args[0]);
+        if (value < 0 || value > 255) {
+          throw new ArithmeticException("Value is out of range");
         }
+       
+        System.out.write(value);
+        System.out.flush();
       }
+    }
     
 }
